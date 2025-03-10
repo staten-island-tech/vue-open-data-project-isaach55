4f5fd7ba960a2ce864e69253e63dbc6cce43bd0b
isaach55 <isaach55@nycstudents.net>
2025-03-10 12:31:08 -0400

    !!!!!!dgsdDhDFHfdhf why is there a 4 and a half hour assembly on wednesday.
    gonna crashout fr

---
+++ vue-open-data-project-isaach55/src/components/DataSet.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const restaurantData = ref('')
+async function getData() {
+  let res = await fetch(https://data.cityofnewyork.us/resource/43nn-pn8j.json)
+  let data = await res.json
+  //samantha rat core
+  //d3.js for maps?
+}
+</script>
+
+<style scoped></style>
