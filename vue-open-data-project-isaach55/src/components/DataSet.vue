93e3f4dcb359519a8e43c9b350bc97dcc63f4919
isaach55 <isaach55@nycstudents.net>
2025-03-11 12:37:50 -0400

    the api is working!!! yipppiepieieiieie

--- vue-open-data-project-isaach55/src/components/DataSet.vue
+++ vue-open-data-project-isaach55/src/components/DataSet.vue
@@ -5,13 +5,22 @@
 <script setup>
 import { ref } from 'vue'
 
-const restaurantData = ref('')
+const apiURL = `https://data.cityofnewyork.us/resource/43nn-pn8j.json`
+
+//create a new url by filtering
+//https://dev.socrata.com/docs/queries/ documentation !
+
 async function getData() {
-  let res = await fetch(https://data.cityofnewyork.us/resource/43nn-pn8j.json)
-  let data = await res.json
+  const dataURL = `https://data.cityofnewyork.us/resource/43nn-pn8j.json?cuisine_description=Italian&$limit=50000`
+  let res = await fetch(dataURL)
+  let data = await res.json()
+  console.log(data)
   //samantha rat core
   //d3.js for maps?
+  //"autofilling" search bar
 }
+
+getData()
 </script>
 
 <style scoped></style>
